fa9ad55fca10ecb1f8ec4952bbe5d7c643d42661
unknown <aeartixenter@gmail.com>
2020-08-26 23:26:23 +0800

    build test

---
+++ build/precache-manifest.95c625c66ef5f59450b529b8dcd71b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c82d20ab983cb12383db6ce6fe92eb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dc335e863325a75bbe6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f347ae63ab28a51ae0af",
+    "url": "/static/js/2.90285c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90285c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc335e863325a75bbe6",
+    "url": "/static/js/main.21c5bdf8.chunk.js"
+  },
+  {
+    "revision": "8156aede69d6734c7bf4",
+    "url": "/static/js/runtime-main.1fb3056a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
